573cef70788d99b0c15f0ea1f6341462f275de8f
Gal Oren <galtrainers@gmail.com>
2021-09-30 10:23:31 +0300

    SincNet

---
+++ src/main.py
@@ -0,0 +1,161 @@
+import yaml
+import typing as tp
+import os
+
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim
+# import wandb  # sudo apt install libpython3.7-dev python3.7 -m pip install wandb
+import numpy as np
+
+from datasets.timit import TimitTrain, TimitEval
+from model.model import MS_SincResNet, SincNet
+from utils import NestedNamespace, compute_chunk_info
+import matplotlib.pyplot as plt
+
+
+def PlotAccuracy(Accuracy):
+    print(Accuracy)
+    len_Accuracy = range(len(Accuracy))
+    plt.plot(len_Accuracy, Accuracy, label='Accuracy', color='green')
+    plt.xlabel('epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='lower left')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.title('Accuracy')
+    path = "currentAccuracy.png"
+    plt.savefig(path)
+    plt.show()
+    plt.close()
+
+
+def PlotLoss(loss):
+    print(loss)
+    len_loss = range(len(loss))
+    plt.plot(len_loss, loss, label='Loss', color='red')
+    plt.xlabel('epoch')
+    plt.ylabel('Loss')
+    plt.legend(loc='lower left')
+    plt.grid(True)
+    plt.tight_layout()
+    plt.title('Loss')
+    path = "currentLoss.png"
+    plt.savefig(path)
+    plt.show()
+    plt.close()
+
+
+def compute_accuracy(logits: torch.Tensor, labels: tp.Union[torch.Tensor, int]) -> float:
+    # 0-1
+    return torch.mean((torch.argmax(logits, dim=1) == labels).float()).item()
+
+
+def main(params: NestedNamespace):
+    TestAcc = []
+    LossPlot = []
+    chunk_len, chunk_shift = compute_chunk_info(params)
+    dataset_train = TimitTrain(params.data.timit.path, chunk_len=chunk_len)
+    dataset_evaluation = TimitEval(
+        params.data.timit.path, chunk_len, chunk_shift, 'test.scp')
+    dataloader = DataLoader(dataset_train, batch_size=params.batch_size,
+                            shuffle=True, drop_last=True)
+
+    # sinc_net = MS_SincResNet()
+    sinc_net = SincNet(
+        chunk_len, params.data.timit.n_classes, params.model.type)
+    sinc_net = sinc_net.to(params.device)
+    optim = torch.optim.RMSprop(
+        sinc_net.parameters(), lr=params.lr, alpha=0.95, eps=1e-8)
+    prev_epoch = 0
+    if params.model.pretrain is not None:
+        checkpoint = torch.load(params.model.pretrain,
+                                map_location=torch.device(params.device))
+        sinc_net.load_state_dict(checkpoint['model_state_dict'])
+        optim.load_state_dict(checkpoint['optimizer_state_dict'])
+        prev_epoch = checkpoint['epoch'] + 1
+    criterion = nn.CrossEntropyLoss()
+
+    for i in range(prev_epoch, prev_epoch + params.n_epochs):
+        accuracy, losses = [], []
+        sinc_net.train()
+        for j, batch in enumerate(dataloader):
+            optim.zero_grad()
+            chunks, labels = batch
+            chunks, labels = chunks.to(params.device), labels.to(params.device)
+            logits = sinc_net(chunks)
+            loss = criterion(logits, labels)
+            loss.backward()
+            optim.step()
+
+            if i % params.verbose_every == 0:
+                losses.append(loss.item())
+                accuracy.append(compute_accuracy(logits, labels))
+        print(f"epoch {i} ")
+
+        if i % params.verbose_every == 0:
+            sinc_net.eval()
+            with torch.no_grad():
+                chunks_accuracy, losses_test = [], []
+                wavs_accuracy = 0
+                for chunks, label, n_chunks in dataset_evaluation:
+                    chunks = chunks.to(params.device)
+                    logits = sinc_net(chunks)
+                    loss = criterion(logits, torch.Tensor(
+                        [label] * n_chunks).long().to(params.device))
+                    losses_test.append(loss.item())
+                    chunks_accuracy.append(
+                        compute_accuracy(logits, label))  # 0-1
+                    wavs_accuracy += (torch.argmax(logits.sum(dim=0))  # 0 or 1
+                                      == label).item()
+                TestAcc.append(wavs_accuracy/len(dataset_evaluation))
+                LossPlot.append(np.mean(losses_test))
+                print(f'epoch {i}\ntrain accuracy {np.mean(accuracy*100)}%\ntrain loss {np.mean(losses)} \n'
+                      f'test loss {LossPlot[-1]}%\n'
+                      f'test wav accuracy {(TestAcc[-1])*100}%')
+                if len(TestAcc) > 1:
+                    if TestAcc[-1] > TestAcc[-2]:
+                        torch.save(
+                            {'model_state_dict': sinc_net.state_dict(
+                            ), 'optimizer_state_dict': optim.state_dict(), 'epoch': i},
+                            os.path.join(params.save_path, params.model.type+'.pt'))
+                        print("saved model!! ")
+    PlotAccuracy(TestAcc)
+    PlotLoss(LossPlot)
+
+
+if __name__ == "__main__":
+    with open('configs/cfg.yaml') as config:
+        params = yaml.load(config, Loader=yaml.FullLoader)
+        params = NestedNamespace(params)
+    if params.model.type not in ['cnn', 'sinc']:
+        raise ValueError("Only two models are supported, use cnn or sinc.")
+    if params.use_wandb:
+        id = wandb.util.generate_id()
+        print("id", id)
+        wandb.init(project='SincNet', id=id, resume="allow",
+                   config={'model type': params.model.type})
+    main(params)
+
+
+# def save_checkpoint(G,D,epoch,trained):
+#     print("=> Saving checkpoint")
+#     save_dir_g = os.path.join(G.model['General']['save_dir'], G.model['name']+ '_G.pt')
+#     save_dir_d = os.path.join(D.model['General']['save_dir'], D.model['name']+ '_D.pt')
+#     torch.save({'G_Model': G,'epoch': epoch,'trained':trained}, save_dir_g)
+#     torch.save({'D_model':D}, save_dir_d)
+
+
+# def load_checkpoint(load_dir,name):
+#     load_dir_g= os.path.join(load_dir, name+'_G.pt')
+#     load_dir_d= os.path.join(load_dir, name+'_D.pt')
+#     print("=> Loading checkpoint")
+#     checkpoint_G = torch.load(load_dir_g)
+#     checkpoint_D = torch.load(load_dir_d)
+#     gen,disc=checkpoint_G['G_Model'],checkpoint_D['D_model']
+#     epoch,trained = checkpoint_G['epoch'],  checkpoint_G['trained']
+#     if trained:
+#         print('load model from file! validate on new genrated images :)\n')
+#     else:
+#         print('continue to train from epoch {}, see log dir for history :)\n'.format(epoch))
+#     return  gen,disc, epoch,trained
---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import typing as tp
+from types import SimpleNamespace
+
+import torch
+import yaml
+
+from model.model import SincNet
+
+
+class NestedNamespace(SimpleNamespace):
+    def __init__(self, dictionary, **kwargs):
+        super().__init__(**kwargs)
+        for key, value in dictionary.items():
+            if isinstance(value, dict):
+                self.__setattr__(key, NestedNamespace(value))
+            else:
+                self.__setattr__(key, value)
+
+
+def compute_chunk_info(params: NestedNamespace) -> tp.Tuple[int, int]:
+    chunk_len = int(params.sample_rate * params.chunk_len_ratio)
+    chunk_shift = int(params.sample_rate * params.chunk_shift_ratio)
+    return chunk_len, chunk_shift
+
+
+def get_params(path_to_cfg: str):
+    with open(path_to_cfg) as config:
+        params = yaml.load(config, Loader=yaml.FullLoader)
+        params = NestedNamespace(params)
+    return params
+
+
+def load_model(params, args, chunk_len):
+    sinc_net = SincNet(chunk_len, params.data.timit.n_classes, args.model_type)
+    checkpoint = torch.load(args.pretrained_model, map_location=torch.device(params.device))
+    sinc_net.load_state_dict(checkpoint['model_state_dict'])
+    sinc_net = sinc_net.to(params.device)
+    sinc_net.eval()
+    return sinc_net
